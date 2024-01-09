2c7e160acc3d577a86f929c55dbc11eab149660a
thaihunghung <110120031@st.tvu.edu.vn>
2024-01-09 15:59:42 +0700

    hung

---
+++ travel/travel/Template.Master.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+
+namespace travel
+{ 
+    public partial class Template : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string[] blogIdArray = GetBlogIds();
+
+            RegisterBlogIdsScript(blogIdArray);
+        }
+
+        private string[] GetBlogIds()
+        {
+            return new string[] { "hung", "sang", "tài" };
+        }
+
+        private void RegisterBlogIdsScript(string[] blogIdArray)
+        {
+  
+            string jsonBlogIds = JsonConvert.SerializeObject(blogIdArray);
+            string script = $"var blogIds = {jsonBlogIds}; console.log('Blog IDs:', blogIds);";
+
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "blogIdsScript", script, true);
+        }
+
+    }
+}
---
+++ travel/travel/TrangChu.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Template.Master" AutoEventWireup="true" CodeBehind="TrangChu.aspx.cs" Inherits="travel.TrangChu" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div>
+        hung
+    </div>
+</asp:Content>
