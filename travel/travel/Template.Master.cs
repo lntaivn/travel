70ffb709b267bedde05963b9f58efe2c508482a4
taiz <llamttai@gmail.com>
2024-01-10 08:49:33 +0700

    commit to tai

--- travel/travel/Template.Master.cs
+++ travel/travel/Template.Master.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Newtonsoft.Json;
 
+
 namespace travel
-{ 
+{
     public partial class Template : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
@@ -15,16 +19,36 @@ namespace travel
             string[] blogIdArray = GetBlogIds();
 
             RegisterBlogIdsScript(blogIdArray);
-        }
 
+        }
+        
         private string[] GetBlogIds()
         {
-            return new string[] { "hung", "sang", "tài" };
+            Response.Write("dc");
+            String scnn = ConfigurationManager.AppSettings["conn"].ToString();
+            SqlConnection conn = new SqlConnection(scnn);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM location", conn); // Sửa câu lệnh SQL ở đây
+
+            DataTable dt = new DataTable();
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+
+            // Tạo một List để chứa các tên địa điểm
+            List<string> locationNames = new List<string>();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                locationNames.Add(row["name"].ToString());
+            }
+
+            return locationNames.ToArray();
         }
+
 
         private void RegisterBlogIdsScript(string[] blogIdArray)
         {
-  
+
             string jsonBlogIds = JsonConvert.SerializeObject(blogIdArray);
             string script = $"var blogIds = {jsonBlogIds}; console.log('Blog IDs:', blogIds);";
 
--- travel/travel/Template.Master.designer.cs
+++ travel/travel/Template.Master.designer.cs
@@ -49,5 +49,14 @@ namespace travel
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+
+        /// <summary>
+        /// markdownContentPlaceholder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder markdownContentPlaceholder;
     }
 }
